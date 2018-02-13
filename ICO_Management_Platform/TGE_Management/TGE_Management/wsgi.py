4e4f023f61d484d0ec1531ffc64985657067ed93
Valérie <samuelpilcer@gmail.com>
2018-02-13 22:10:56 +0100

    Create django webapp

---
+++ ICO_Management_Platform/TGE_Management/TGE_Management/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TGE_Management project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TGE_Management.settings")
+
+application = get_wsgi_application()
