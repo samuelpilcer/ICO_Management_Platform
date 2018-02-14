eff0931bc075950d3d4820f035d28162d1f397ba
SamuelPilcer <samuelpilcer@gmail.com>
2018-02-14 09:43:20 +0100

    Update token model

--- ICO_Management_Platform/TGE_Management/tokens/models.py
+++ ICO_Management_Platform/TGE_Management/tokens/models.py
@@ -6,7 +6,10 @@ from django.contrib.auth.models import User
 
 class Token(models.Model):
     titre = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=100)
     description = models.CharField(max_length=400)
+    cap = models.IntegerField(default=1000000)
+    ether_price = models.FloatField(default=1)
     admin = models.ForeignKey(User)
     date = models.DateTimeField(auto_now_add=True, auto_now=False, 
                                 verbose_name="Date de parution")
