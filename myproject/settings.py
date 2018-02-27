0c83b19be13cc45954b0c66930b6491e3069ed61
Valérie <samuelpilcer@gmail.com>
2018-02-27 19:52:04 +0100

    First commit, to test heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,11 +11,13 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+PROJECT_DIR = Path(__file__).parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [PROJECT_DIR.child('templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ myproject/views.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+import requests
+import json
+import csv
+import os
+from django.views.static import serve
+from .forms import ConnexionForm, InscriptionForm
+import sys
+sys.path.append('../')
+from tokens.models import Token
+
+def home(request):
+    try:
+        models = Token.objects.all().filter(admin=request.user)
+    except:
+        models = []
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'index.html', locals())
