dd52a99df79a8749197518ed031e738a8afd205f
Valérie <samuelpilcer@gmail.com>
2018-02-27 22:41:03 +0100

    Change Django version

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-#from django.urls import path, include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
