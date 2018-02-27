967455919c154ca99041ade1f078c022e838fbe4
Valérie <samuelpilcer@gmail.com>
2018-02-27 20:28:18 +0100

    First commit, to test heroku

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import url, admin
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.home, name='home'),
+    path(r'^$', views.home, name='home'),
 ]
