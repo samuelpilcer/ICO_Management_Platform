677e18fa120d5013991ebc74ea79abd1aaf39d68
Valérie <samuelpilcer@gmail.com>
2018-02-27 20:31:13 +0100

    First commit, to test heroku

--- myproject/views.py
+++ myproject/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
