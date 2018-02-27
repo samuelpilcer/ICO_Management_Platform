e18a1f71e825b39271e9819ef1f8351eb5a0f6d8
Valérie <samuelpilcer@gmail.com>
2018-02-27 21:41:44 +0100

    Change Django version

--- myproject/settings.py
+++ myproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [PROJECT_ROOT+'/templates',],
+        'DIRS': ['/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
